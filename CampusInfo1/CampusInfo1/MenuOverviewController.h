273a34bead0c62d9586c485e963dc95e57231588
Ilka <koke@zhaw.ch>
2013-08-07 21:11:37 +0200

    Adding to menu overview

--- CampusInfo1/CampusInfo1/MenuOverviewController.h
+++ CampusInfo1/CampusInfo1/MenuOverviewController.h
@@ -14,11 +14,12 @@
     IBOutlet UITableView        *_menuTableView;
     IBOutlet UITableViewCell    *_menuTimeTableCell;
     IBOutlet UITableViewCell    *_menuMensaTableCell;
-    
+    IBOutlet UITableViewCell    *_menuNewsTableCell;
 }
 
 @property (nonatomic, retain) IBOutlet UITableView               *_menuTableView;
 @property (nonatomic, retain) IBOutlet UITableViewCell           *_menuTimeTableCell;
 @property (nonatomic, retain) IBOutlet UITableViewCell           *_menuMensaTableCell;
+@property (nonatomic, retain) IBOutlet UITableViewCell           *_menuNewsTableCell;
 
 @end
--- CampusInfo1/CampusInfo1/MenuOverviewController.m
+++ CampusInfo1/CampusInfo1/MenuOverviewController.m
@@ -16,6 +16,7 @@
 @synthesize _menuTableView;
 @synthesize _menuTimeTableCell;
 @synthesize _menuMensaTableCell;
+@synthesize _menuNewsTableCell;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -42,6 +43,7 @@
     _menuTableView = nil;
     _menuTimeTableCell = nil;
     _menuMensaTableCell = nil;
+    _menuNewsTableCell = nil;
     [super viewDidUnload];
 }
 
@@ -57,12 +59,47 @@
     [self dismissModalViewControllerAnimated:YES];
 }
 
+-(void) moveToOev:(id)sender event:(id)event
+{
+    NSLog(@"move to ÖV");
+}
+
+-(void) moveToPerson:(id)sender event:(id)event
+{
+    NSLog(@"move to person");
+}
+
+-(void) moveToContacts:(id)sender event:(id)event
+{
+    NSLog(@"move to contact");
+}
+
+-(void) moveToQuestion:(id)sender event:(id)event
+{
+    NSLog(@"move to question");
+}
+
+-(void) moveToNews:(id)sender event:(id)event
+{
+    NSLog(@"move to news");
+}
+
+-(void) moveToEvents:(id)sender event:(id)event
+{
+    NSLog(@"move to events");
+}
+
+-(void) moveToSettings:(id)sender event:(id)event
+{
+    self.tabBarController.selectedIndex = 4;
+    [self dismissModalViewControllerAnimated:YES];
+}
 
 // table and table cell handling
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 5;
+    return 3;
 }
 
 
@@ -74,7 +111,7 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return 44;
+    return 72;
 }
 
 
@@ -98,15 +135,36 @@
             _cell = _menuTimeTableCell;
             self._menuTimeTableCell = nil;
         }
-        UIButton *_timeTableButton  = (UIButton *) [_cell viewWithTag:1];
-        _timeTableButton.enabled = true;
-        [_timeTableButton addTarget:self action:@selector(moveToTimeTable:event:) forControlEvents:UIControlEventTouchUpInside];
+        
+        // time table
+        UIButton *_timeTableIconButton  = (UIButton *) [_cell viewWithTag:1];
+        _timeTableIconButton.enabled = true;
+        [_timeTableIconButton addTarget:self action:@selector(moveToTimeTable:event:) forControlEvents:UIControlEventTouchUpInside];
+        UIButton *_timeTableTitleButton  = (UIButton *) [_cell viewWithTag:2];
+        _timeTableTitleButton.enabled = true;
+        [_timeTableTitleButton addTarget:self action:@selector(moveToTimeTable:event:) forControlEvents:UIControlEventTouchUpInside];
+        
+        // mensa
+        UIButton *_mensaIconButton  = (UIButton *) [_cell viewWithTag:3];
+        _mensaIconButton.enabled = true;
+        [_mensaIconButton addTarget:self action:@selector(moveToMensa:event:) forControlEvents:UIControlEventTouchUpInside];
+        UIButton *_mensaTitleButton  = (UIButton *) [_cell viewWithTag:4];
+        _mensaTitleButton.enabled = true;
+        [_mensaTitleButton addTarget:self action:@selector(moveToMensa:event:) forControlEvents:UIControlEventTouchUpInside];
+        
+        //OeV
+        UIButton *_oevIconButton  = (UIButton *) [_cell viewWithTag:5];
+        _oevIconButton.enabled = true;
+        [_oevIconButton addTarget:self action:@selector(moveToOev:event:) forControlEvents:UIControlEventTouchUpInside];
+        UIButton *_oevTitleButton  = (UIButton *) [_cell viewWithTag:6];
+        _oevTitleButton.enabled = true;
+        [_oevTitleButton addTarget:self action:@selector(moveToOev:event:) forControlEvents:UIControlEventTouchUpInside];
         
         _cell.contentView.backgroundColor = _lectureBackgroundColor;
         _cell.backgroundColor = _cell.contentView.backgroundColor;
     }
-    else
-    //if (_cellSelection > 0)
+    
+    if (_cellSelection == 1)
     {
         _cellIdentifier  = @"MenuMensaTableCell";
         _cell            = [tableView dequeueReusableCellWithIdentifier:_cellIdentifier];
@@ -116,15 +174,75 @@
             _cell = _menuMensaTableCell;
             self._menuMensaTableCell = nil;
         }
-        UIButton *_mensaButton  = (UIButton *) [_cell viewWithTag:1];
-        _mensaButton.enabled = true;
-        [_mensaButton addTarget:self action:@selector(moveToMensa:event:) forControlEvents:UIControlEventTouchUpInside];
+        
+        // person
+        UIButton *_personIconButton  = (UIButton *) [_cell viewWithTag:1];
+        _personIconButton.enabled = true;
+        [_personIconButton addTarget:self action:@selector(moveToPerson:event:) forControlEvents:UIControlEventTouchUpInside];
+        UIButton *_personTitleButton  = (UIButton *) [_cell viewWithTag:2];
+        _personTitleButton.enabled = true;
+        [_personTitleButton addTarget:self action:@selector(moveToPerson:event:) forControlEvents:UIControlEventTouchUpInside];
+        
+        // contact
+        UIButton *_contactsIconButton  = (UIButton *) [_cell viewWithTag:3];
+        _contactsIconButton.enabled = true;
+        [_contactsIconButton addTarget:self action:@selector(moveToContacts:event:) forControlEvents:UIControlEventTouchUpInside];
+        UIButton *_contactsTitleButton  = (UIButton *) [_cell viewWithTag:4];
+        _contactsTitleButton.enabled = true;
+        [_contactsTitleButton addTarget:self action:@selector(moveToContacts:event:) forControlEvents:UIControlEventTouchUpInside];
+        
+        //???
+        UIButton *_questionIconButton  = (UIButton *) [_cell viewWithTag:5];
+        _questionIconButton.enabled = true;
+        [_questionIconButton addTarget:self action:@selector(moveToQuestion:event:) forControlEvents:UIControlEventTouchUpInside];
+        UIButton *_questionTitleButton  = (UIButton *) [_cell viewWithTag:6];
+        _questionTitleButton.enabled = true;
+        [_questionTitleButton addTarget:self action:@selector(moveToQuestion:event:) forControlEvents:UIControlEventTouchUpInside];
 
         _cell.contentView.backgroundColor = _lectureBackgroundColor;
         _cell.backgroundColor = _cell.contentView.backgroundColor;
     }
     
-    NSLog(@"_cellSelection: %i", _cellSelection);
+    if (_cellSelection == 2)
+    {
+        _cellIdentifier  = @"MenuNewsTableCell";
+        _cell            = [tableView dequeueReusableCellWithIdentifier:_cellIdentifier];
+        if (_cell == nil)
+        {
+            [[NSBundle mainBundle] loadNibNamed:@"MenuNewsTableCell" owner:self options:nil];
+            _cell = _menuNewsTableCell;
+            self._menuNewsTableCell = nil;
+        }
+        
+        // news
+        UIButton *_newsIconButton  = (UIButton *) [_cell viewWithTag:1];
+        _newsIconButton.enabled = true;
+        [_newsIconButton addTarget:self action:@selector(moveToNews:event:) forControlEvents:UIControlEventTouchUpInside];
+        UIButton *_newsTitleButton  = (UIButton *) [_cell viewWithTag:2];
+        _newsTitleButton.enabled = true;
+        [_newsTitleButton addTarget:self action:@selector(moveToNews:event:) forControlEvents:UIControlEventTouchUpInside];
+        
+        // events
+        UIButton *_eventsIconButton  = (UIButton *) [_cell viewWithTag:3];
+        _eventsIconButton.enabled = true;
+        [_eventsIconButton addTarget:self action:@selector(moveToEvents:event:) forControlEvents:UIControlEventTouchUpInside];
+        UIButton *_eventsTitleButton  = (UIButton *) [_cell viewWithTag:4];
+        _eventsTitleButton.enabled = true;
+        [_eventsTitleButton addTarget:self action:@selector(moveToEvents:event:) forControlEvents:UIControlEventTouchUpInside];
+        
+        // settings
+        UIButton *_settingsIconButton  = (UIButton *) [_cell viewWithTag:5];
+        _settingsIconButton.enabled = true;
+        [_settingsIconButton addTarget:self action:@selector(moveToSettings:event:) forControlEvents:UIControlEventTouchUpInside];
+        UIButton *_settingsTitleButton  = (UIButton *) [_cell viewWithTag:6];
+        _settingsTitleButton.enabled = true;
+        [_settingsTitleButton addTarget:self action:@selector(moveToSettings:event:) forControlEvents:UIControlEventTouchUpInside];
+        
+        _cell.contentView.backgroundColor = _lectureBackgroundColor;
+        _cell.backgroundColor = _cell.contentView.backgroundColor;
+    }
+    
+    //NSLog(@"_cellSelection: %i", _cellSelection);
     return _cell;
 }
 
