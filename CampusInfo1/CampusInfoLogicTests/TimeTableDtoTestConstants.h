20e59ee2d52efdb663c940a703d50d7d5042ac22
Ilka <koke@zhaw.ch>
2013-09-21 17:39:48 +0200

    Tests for timetable dto

---
+++ CampusInfo1/CampusInfoLogicTests/TimeTableDtoTestConstants.h
@@ -0,0 +1,103 @@
+//
+//  TimeTableDtoTestConstants.h
+//  CampusInfo1
+//
+//  Created by Ilka Kokemor on 20.09.13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TimeTableDtoTestConstants : NSObject
+
+extern NSString * const day1           			;
+extern NSString * const day2           			;
+
+extern NSString * const startTime1            ;
+extern NSString * const endTime1              ;
+
+extern NSString * const startTime2            ;
+extern NSString * const endTime2             ;
+
+extern NSString * const startTime3            ;
+extern NSString * const endTime3              ;
+
+extern NSString * const startTime4            ;
+extern NSString * const endTime4             ;
+
+extern NSString * const startTime5            ;
+extern NSString * const endTime5              ;
+
+extern NSString * const startTime6            ;
+extern NSString * const endTime6              ;
+
+extern NSString * const day1StartTime1 			;
+extern NSString * const day1EndTime1   			;
+
+extern NSString * const day1StartTime2 			;
+extern NSString * const day1EndTime2   			;
+
+extern NSString * const day1StartTime3 			;
+extern NSString * const day1EndTime3   			;
+
+extern NSString * const day1StartTime4 			;
+extern NSString * const day1EndTime4   			;
+
+extern NSString * const day1StartTime5 			;
+extern NSString * const day1EndTime5   			;
+
+extern NSString * const day1StartTime6 			;
+extern NSString * const day1EndTime6   			;
+
+extern NSString * const lectureDescription1 ;
+extern NSString * const lectureName1        ;
+
+extern NSString * const lectureDescription2 ;
+extern NSString * const lectureName2        ;
+
+extern NSString * const lectureDescription3 ;
+extern NSString * const lectureName3        ;
+
+extern NSString * const lectureDescription4 ;
+extern NSString * const lectureName4        ;
+
+extern NSString * const lectureDescription5 ;
+extern NSString * const lectureName5        ;
+
+extern NSString * const lectureDescription6 ;
+extern NSString * const lectureName6        ;
+
+extern NSString * const lecturerPrename1    ;
+extern NSString * const lecturerName1       ;
+extern NSString * const lecturerAcronym1    ;
+
+extern NSString * const lecturerPrename2    ;
+extern NSString * const lecturerName2       ;
+extern NSString * const lecturerAcronym2    ;
+
+extern NSString * const lecturerPrename3    ;
+extern NSString * const lecturerName3       ;
+extern NSString * const lecturerAcronym3    ;
+
+extern NSString * const lecturerPrename4    ;
+extern NSString * const lecturerName4       ;
+extern NSString * const lecturerAcronym4    ;
+
+extern NSString * const roomName1           ;
+extern NSString * const roomName2           ;
+extern NSString * const roomName3           ;
+extern NSString * const roomName4           ;
+extern NSString * const roomName5           ;
+extern NSString * const roomName6           ;
+
+extern NSString * const className1          ;
+extern NSString * const className2          ;
+extern NSString * const className3          ;
+extern NSString * const className4          ;
+extern NSString * const className5          ;
+extern NSString * const className6          ;
+
+extern NSString * const departementName1;
+extern NSString * const departementName2;
+
+@end
---
+++ CampusInfo1/CampusInfoLogicTests/TimeTableDtoTestConstants.m
@@ -0,0 +1,105 @@
+//
+//  TimeTableDtoTestConstants.m
+//  CampusInfo1
+//
+//  Created by Ilka Kokemor on 20.09.13.
+//
+//
+
+#import "TimeTableDtoTestConstants.h"
+
+@implementation TimeTableDtoTestConstants
+
+NSString * const day1           		= @"2013-08-13";
+NSString * const day2           		= @"2013-08-14";
+
+NSString * const startTime1             = @"08:00";
+NSString * const endTime1               = @"08:45";
+
+NSString * const startTime2             = @"08:50";
+NSString * const endTime2              = @"09:35";
+
+NSString * const startTime3             = @"10:00";
+NSString * const endTime3               = @"10:45";
+
+NSString * const startTime4             = @"10:50";
+NSString * const endTime4              = @"11:35";
+
+NSString * const startTime5             = @"14:00";
+NSString * const endTime5               = @"14:45";
+
+NSString * const startTime6             = @"14:50";
+NSString * const endTime6               = @"15:35";
+
+
+NSString * const day1StartTime1 		= @"2013-08-13 08:00";
+NSString * const day1EndTime1   		= @"2013-08-13 08:45";
+
+NSString * const day1StartTime2 		= @"2013-08-13 08:50";
+NSString * const day1EndTime2   		= @"2013-08-13 09:35";
+
+NSString * const day1StartTime3 		= @"2013-08-13 10:00";
+NSString * const day1EndTime3   		= @"2013-08-13 10:45";
+
+NSString * const day1StartTime4 		= @"2013-08-13 10:50";
+NSString * const day1EndTime4   		= @"2013-08-13 11:35";
+
+NSString * const day1StartTime5 		= @"2013-08-13 14:00";
+NSString * const day1EndTime5   		= @"2013-08-13 14:45";
+
+NSString * const day1StartTime6 		= @"2013-08-13 14:50";
+NSString * const day1EndTime6   		= @"2013-08-13 15:35";
+
+NSString * const lectureDescription1    = @"Java Vorlesung";
+NSString * const lectureName1           = @"t.jff-v";
+
+NSString * const lectureDescription2    = @"Java Praktikum";
+NSString * const lectureName2           = @"t.jff-p";
+
+NSString * const lectureDescription3    = @"Mathematik für Ingenieure 1";
+NSString * const lectureName3           = @"t.mae1-v";
+
+NSString * const lectureDescription4    = @"Mathematik für Ingenieure 2";
+NSString * const lectureName4           = @"t.mae2-v";
+
+NSString * const lectureDescription5    = @"Software Engineering 1 Praktikum";
+NSString * const lectureName5           = @"t.swe1-p";
+
+NSString * const lectureDescription6    = @"Software Engineering 1 Vorlesung";
+NSString * const lectureName6           = @"t.swe1-v";
+
+NSString * const lecturerPrename1       = @"Hans";
+NSString * const lecturerName1          = @"Muster";
+NSString * const lecturerAcronym1       = @"muha";
+
+NSString * const lecturerPrename2       = @"Erika";
+NSString * const lecturerName2          = @"Master";
+NSString * const lecturerAcronym2       = @"maer";
+
+NSString * const lecturerPrename3       = @"Heinrich";
+NSString * const lecturerName3          = @"Mueller";
+NSString * const lecturerAcronym3       = @"muhe";
+
+NSString * const lecturerPrename4       = @"Ulrike";
+NSString * const lecturerName4          = @"Meier";
+NSString * const lecturerAcronym4       = @"meul";
+
+NSString * const roomName1              = @"TE 123";
+NSString * const roomName2              = @"TB 111";
+NSString * const roomName3              = @"TH 404";
+NSString * const roomName4              = @"TE 555";
+NSString * const roomName5              = @"TG 321";
+NSString * const roomName6              = @"TD 876";
+
+NSString * const className1             = @"T_IT13a_WIN.BA";
+NSString * const className2             = @"T_IT13a_ZH.BA";
+NSString * const className3             = @"T_MT13a.BA";
+NSString * const className4             = @"T_MT13b.BA";
+NSString * const className5             = @"T_WI12a.BA";
+NSString * const className6             = @"T_WI12b.BA";
+
+NSString * const departementName1       = @"T";
+NSString * const departementName2       = @"L_IUED";
+
+
+@end
